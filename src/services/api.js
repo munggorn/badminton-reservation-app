6063a9f15fcba2b4a4c8f20b6422b9cf533223d9
munggorn <128501902+munggorn@users.noreply.github.com>
2024-12-09 15:01:57 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const API_URL = 'https://badmindton-reservation-backend-48221d08d8e7.herokuapp.com/api';
-const SOCKET_URL = 'https://badmindton-reservation-backend-48221d08d8e7.herokuapp.com';
+const API_URL = 'https://badminton-reservation-59c4db5dc0dc.herokuapp.com/api';
+const SOCKET_URL = 'https://badminton-reservation-59c4db5dc0dc.herokuapp.com';
 
 const api = axios.create({
   baseURL: API_URL,
