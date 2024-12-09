89296b28ae5f97d2bf97c85d7418a4ffba1624ac
munggorn <128501902+munggorn@users.noreply.github.com>
2024-12-09 11:20:09 +0700

    Update BadmintonReservation.js

--- src/components/BadmintonReservation.js
+++ src/components/BadmintonReservation.js
@@ -43,14 +43,10 @@ const BadmintonReservation = () => {
       const response = await axios.get(`${API_URL}/reservations`);
       const reservationData = {};
       response.data.forEach(reservation => {
-        // Get court number whether courtId is an object or a direct value
-        const courtNumber = typeof reservation.courtId === 'object' 
-          ? reservation.courtId.courtNumber 
-          : reservation.courtId;
-  
-        reservationData[`${courtNumber}-${reservation.timeSlot}`] = {
+        // Just use courtId directly since it's not an object with courtNumber
+        reservationData[`${reservation.courtId}-${reservation.timeSlot}`] = {
           id: reservation._id,
-          courtId: courtNumber,
+          courtId: reservation.courtId,
           timeSlot: reservation.timeSlot,
           name: reservation.userName,
           partyNames: reservation.partyNames
