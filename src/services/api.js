f5974234b853a98d4d59cd11eb6aca7bbb8841a4
munggorn <128501902+munggorn@users.noreply.github.com>
2024-12-09 11:27:12 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,32 @@
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const API_URL = 'https://badminton-reservation-59c4db5dc0dc.herokuapp.com/api';
-const SOCKET_URL = 'https://badminton-reservation-59c4db5dc0dc.herokuapp.com/api';
+const BASE_URL = 'https://badminton-reservation-59c4db5dc0dc.herokuapp.com';
+const API_URL = `${BASE_URL}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // This is important for CORS with credentials
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export const socket = io(SOCKET_URL, {
-  withCredentials: true, // This matches the CORS setting on the server
+export const socket = io(BASE_URL, {
+  withCredentials: true,
+  reconnectionDelay: 1000,
+  reconnection: true,
+  reconnectionAttempts: 10,
+  transports: ['websocket'],
+  agent: false,
+  upgrade: false,
+  rejectUnauthorized: false
 });
 
 export const getCourts = () => api.get('/courts');
 export const createReservation = (reservationData) => api.post('/reservations', reservationData);
 export const getReservations = () => api.get('/reservations');
 
-// Add error interceptor
 api.interceptors.response.use(
   response => response,
   error => {
